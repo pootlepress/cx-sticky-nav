9e0e5a07fbdf90563012bb3836677439a3f775ac
Alan Ng <jenfoong@gmail.com>
2015-02-11 14:09:07 +0800

    Fix full footer case

--- pootlepress-sticky-nav-functions.php
+++ pootlepress-sticky-nav-functions.php
@@ -182,6 +182,22 @@ function stickyjs(){
                     }
                 }
 
+                function stickyNavFullFooter() {
+                    if ($(window).scrollTop() > stickNavOffset) {
+                        //Sticky Nav
+                        $('#navhidden').css({'min-height':navhiddenHeight,'margin-bottom':navhiddenMargin});
+                        $('#navhidden').show();
+                        $('#navigation').css(<?php echo $stickyStyle; ?>);
+                        $('#navigation').css({left: '50%', transform: ''});
+                        $('#navigation').attr('style', $('#navigation').attr('style') + ' transform: translateX(-50%) !important');
+                    } else {
+                        //Normal Nav
+                        $('#navhidden').hide();
+                        $('#navigation').css(<?php echo $normalStyle; ?>);
+                        $('#navigation').css({left: '', transform: ''});
+                    }
+                }
+
                 function stickyNavFull() {
                     var innerWidth = contentWidth;
                     if ($(window).scrollTop() > stickNavOffset) {
@@ -218,8 +234,11 @@ function stickyjs(){
                     } else if ($('#nav-container').length > 0) {
                         //nav-container exists
                         stickyNavFull();
-                    } else if ((contentWidth > 959)&&(navWidth < 961)) { //Business Slider Fix
-                        stickyNavBusiness();
+                    } else if ($('body').is('.full-width')) {
+                        // body is still full width, probably because it is full footer
+                        stickyNavFullFooter();
+//                    } else if ((contentWidth > 959)&&(navWidth < 961)) { //Business Slider Fix
+//                        stickyNavBusiness();
                     } else {
                         stickyNav();
                     }
